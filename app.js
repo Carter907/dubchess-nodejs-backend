8de15b701a8f721dd904e948ea9b44df8caaa440
carter <speers.carter@gmail.com>
2024-01-30 17:07:23 -0500

    new stuff

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+//
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/apps/dubchess/api', (req, res) => {
+    res.set('Content-Type', 'text/html; charset=utf-8');
+    res.send('<h1> Hello World </h1>');
+});
+app.listen(port, () => {
+    console.log(`Example app listening!!`);
+});
