83a72607fd52c222e23a3fc91592d4f36f513c08
carter <speers.carter@gmail.com>
2024-01-30 19:15:18 -0500

    text change

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.get('/apps/dubchess/api', (req: any, res: any) => {
     res.set('Content-Type', 'text/html; charset=utf-8');
-    res.send('<h1> Hello World </h1>');
+    res.send('<h1> Yo this is from the backend </h1>');
 });
 app.listen(port, () => {
     console.log(`Example app listening!!`);
