e1996c6a46d575c7858862f4eb2eef998fc88a5a
carter <speers.carter@gmail.com>
2024-01-30 19:11:19 -0500

    new project

--- app.ts
+++ app.ts
@@ -1,17 +1,12 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+import express from "express";
 // import { fileURLToPath } from 'url';
 // import { dirname } from 'path';
 //
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = dirname(__filename);
-const app = (0, express_1.default)();
+const app = express();
 const port = process.env.PORT || 3000;
-app.get('/apps/dubchess/api', (req, res) => {
+app.get('/apps/dubchess/api', (req: any, res: any) => {
     res.set('Content-Type', 'text/html; charset=utf-8');
     res.send('<h1> Hello World </h1>');
 });
